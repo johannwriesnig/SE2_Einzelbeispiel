72d7fe0b50fb0e8550c887744f95e34977cd37d3
Johann Wriesnig <jowriesnig@edu.aau.at>
2021-03-09 21:44:52 +0100

    Länge von "inputField" auf tatsächliche Länge einer Matrikelnummer(8) begrenzt.

--- src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.se2_einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
